324a6ab27ff318f56925f412bf7198f0012d7c41
Arno van der Vegt <arnovandervegt@startmail.com>
2016-06-19 10:52:01 +0200

    Check file path.

--- public/js/Files.js
+++ public/js/Files.js
@@ -1,15 +1,16 @@
 (function() {
     var File = Class(function() {
             this.init = function(opts) {
-                this._dir         = opts.dir;
-                this._files        = opts.files;
-                this._name         = opts.name || this._files.createNewName('File');
+                this._dir       = opts.dir;
+                this._local     = opts.local;
+                this._files     = opts.files;
+                this._name      = opts.name || this._files.createNewName('File');
                 this._canRename = ('canRename' in opts) ? opts.canRename : true;
-                this._data         = opts.data || '';
-                this._hasData     = ('data' in opts) || this._dir;
-                this._changed     = ('saved' in opts) ? !opts.saved : true;
-                this._open         = false;
-                this._meta        = {};
+                this._data      = opts.data || '';
+                this._hasData   = ('data' in opts) || this._dir;
+                this._changed   = ('saved' in opts) ? !opts.saved : true;
+                this._open      = false;
+                this._meta      = {};
             };
 
             this.getName = function() {
@@ -43,8 +44,14 @@
                         callback(this._data);
                         return;
                     }
+                    var uri;
+                    if (this._local) {
+                    	uri = '/api/file?filename=' + encodeURIComponent(this._name);
+                    } else {
+                    	uri = 'http://arnovandervegt.github.io/wheel/wheel' + this._name;
+                    }
                     ajaxUtils.send(
-                        '/api/file?filename=' + encodeURIComponent(this._name),
+                        uri,
                         function(error, data) {
                             if (error) {
                                 console.error(error, data);
@@ -166,10 +173,10 @@
                 for (var i = 0; i < files.length; i++) {
                     var file = files[i];
                     this.createFile({
-                        name:         file.name,
-                        dir:         !!file.dir,
-                        hasData:     false,
-                        saved:         true
+                        name:    file.name,
+                        dir:     !!file.dir,
+                        hasData: false,
+                        saved:   true
                     });
                 }
                 this.emit('Loaded');
@@ -192,9 +199,9 @@
             };
 
             this.newName = function(start, end) {
-                var files      = this._files,
-                    name,
-                    index    = 1;
+                var files = this._files;
+                var name;
+                var index = 1;
 
                 while (true) {
                     name = start + index + end;
@@ -217,6 +224,7 @@
 
             this.createFile = function(fileOpts) {
                 fileOpts.files = this;
+                fileOpts.local = this._local;
                 var file = new File(fileOpts);
                 file.save();
                 this._files.push(file);
@@ -224,8 +232,8 @@
             };
 
             this.renameDir = function(oldName, newName) {
-                var files     = this._files,
-                    length     = oldName.length;
+                var files  = this._files;
+                var length = oldName.length;
                 for (var i = 0; i < files.length; i++) {
                     var file = files[i],
                         name = file.getName();
@@ -267,9 +275,9 @@
                             console.error(error, data);
                             this._changed = true;
                         } else {
-                            var length     = name.length,
-                                files     = this._files,
-                                i         = 0;
+                            var length = name.length;
+                            var files  = this._files;
+                            var i      = 0;
 
                             while (i < files.length) {
                                 var filename = files[i].getName();
