feb58c48fbb0fa09d05ee1b91023bf9478339003
Arno van der Vegt <arnovandervegt@startmail.com>
2016-09-16 19:55:25 +0200

    Optimized call.

--- public/js/compiler/commands/Call.js
+++ public/js/compiler/commands/Call.js
@@ -6,19 +6,15 @@
         'compiler.commands.Call',
         wheel.Class(wheel.compiler.commands.CommandCompiler, function(supr) {
             this.compileConstantParameter = function(param, paramInfo, offset) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
 
-                if (paramInfo.metaType === $.T_META_STRING) {
-                    paramInfo.value = compilerData.declareString(paramInfo.value);
-                }
-                compilerOutput.add(compiler.createCommand('set', [{type: $.T_NUM_L, value: offset}, paramInfo]));
+                (paramInfo.metaType === $.T_META_STRING) && (paramInfo.value = compilerData.declareString(paramInfo.value));
+                compilerOutput.a($.set.code, $.LOCAL(offset), paramInfo);
             };
 
             this.compileLocalPoinerParam = function(param, paramInfo, offset, size) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
 
                 compilerOutput.a($.set.code,  $.SRC(),       $.LOCAL(paramInfo.value));
@@ -28,8 +24,7 @@
             };
 
             this.compileLocalParam = function(param, paramInfo, offset) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
                 var vr             = paramInfo.vr;
 
@@ -43,21 +38,15 @@
                     compilerOutput.a($.set.code, $.LOCAL(offset), $.SRC());
                 } else if (paramInfo.metaType === $.T_META_ADDRESS) {
 console.log('Warning!!!');
-                    paramInfo.type  = $.T_NUM_G;
-                    paramInfo.value = $.REG_SRC;
-                    compilerOutput.add(compiler.createCommand('set', [{type: $.T_NUM_L, value: offset}, paramInfo]));
-
-                    if (paramInfo.value) {
-                        compilerOutput.a($.set.code, [{type: $.T_NUM_L, value: offset}, {type: $.T_NUM_C, value: paramInfo.value}]);
-                    }
+                    compilerOutput.a($.set.code, $.LOCAL(offset), $.SRC());
+                    paramInfo.value && compilerOutput.a($.set.code, $.LOCAL(offset), $.CONST(paramInfo.value));
                 } else {
-                    compilerOutput.add(compiler.createCommand('set', [{type: $.T_NUM_L, value: offset}, paramInfo]));
+                    compilerOutput.a($.set.code, $.LOCAL(offset), paramInfo);
                 }
             };
 
             this.compileLocalStructParam = function(param, paramInfo, offset, size) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
                 var vr             = paramInfo.vr;
 
@@ -68,26 +57,16 @@ console.log('Warning!!!');
                 } else if (vr.metaType === $.T_META_POINTER) {
                     this.compileLocalPoinerParam(param, paramInfo, offset, size);
                 } else {
-                    if (paramInfo.value === 0) {
-                        compilerOutput.a($.set.code, $.SRC(), $.STACK());
-                    } else {
-                        compilerOutput.a($.set.code, $.SRC(), $.CONST(paramInfo.value));
-                        compilerOutput.a($.add.code, $.SRC(), $.STACK());
-                    }
-
-                    if (offset === 0) {
-                        compilerOutput.a($.set.code, $.DEST(), $.STACK());
-                    } else {
-                        compilerOutput.a($.set.code, $.DEST(), $.CONST(offset));
-                        compilerOutput.a($.add.code, $.DEST(), $.STACK());
-                    }
+                    compilerOutput.a($.set.code, $.SRC(), $.STACK());
+                    (paramInfo.value === 0) || compilerOutput.a($.add.code, $.SRC(), $.CONST(paramInfo.value));
+                    compilerOutput.a($.set.code, $.DEST(), $.STACK());
+                    (offset === 0) || compilerOutput.a($.add.code, $.DEST(), $.CONST(offset));
                     compilerOutput.a($.copy.code, $.CONST(size), $.CONST(0));
                 }
             };
 
             this.compileGlobalParam = function(param, paramInfo, offset) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
                 var vr             = paramInfo.vr;
 
@@ -96,17 +75,14 @@ console.log('Warning!!!');
                     compilerOutput.a($.set.code,  $.DEST(),   $.CONST(offset));
                     compilerOutput.a($.add.code,  $.DEST(),   $.STACK());
                     compilerOutput.a($.copy.code, $.CONST(1), $.CONST(0));
-                } else if (paramInfo.metaType === $.T_META_ADDRESS) {
-                    paramInfo.type = $.T_NUM_C;
-                    compilerOutput.a($.set.code, [{type: $.T_NUM_L, value: offset}, paramInfo]);
                 } else {
-                    compilerOutput.a($.set.code, [{type: $.T_NUM_L, value: offset}, paramInfo]);
+                    (paramInfo.metaType === $.T_META_ADDRESS) && (paramInfo.type = $.T_NUM_C);
+                    compilerOutput.a($.set.code, $.LOCAL(offset), paramInfo);
                 }
             };
 
             this.compileGlobalStructParam = function(param, paramInfo, offset, size) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
                 var vr             = paramInfo.vr;
 
@@ -124,28 +100,18 @@ console.log('Warning!!!');
                 }
 
                 if (paramInfo.metaType === $.T_META_ADDRESS) {
-                    paramInfo.type = $.T_NUM_C;
-                    compilerOutput.add(compiler.createCommand('set', [{type: $.T_NUM_L, value: offset}, paramInfo]));
+                    compilerOutput.a($.set.code, $.LOCAL(offset), $.CONST(paramInfo.value));
                 } else {
-                    if (paramInfo.metaType === $.T_META_POINTER) {
-                        compilerOutput.a($.set.code, [$.SRC(), {type: $.T_NUM_G, value: paramInfo.value}]);
-                    } else {
-                        compilerOutput.a($.set.code, [$.SRC(), {type: $.T_NUM_C, value: paramInfo.value}]);
-                    }
-
-                    if (offset === 0) {
-                        compilerOutput.a($.set.code, [$.DEST(), $.STACK()]);
-                    } else {
-                        compilerOutput.a($.set.code, [$.DEST(), {type: $.T_NUM_C, value: offset}]);
-                        compilerOutput.a($.add.code, [$.DEST(), $.STACK()]);
-                    }
-                    compilerOutput.a($.copy.code, [{type: $.T_NUM_C, value: size}]);
+                    var ptr = (paramInfo.metaType === $.T_META_POINTER);
+                    compilerOutput.a($.set.code, $.SRC(), ptr ? $.GLOBAL(paramInfo.value) : $.CONST(paramInfo.value));
+                    compilerOutput.a($.set.code, $.DEST(), $.STACK());
+                    (offset === 0) || compilerOutput.a($.add.code, $.DEST(), $.CONST(offset));
+                    compilerOutput.a($.copy.code, $.CONST(size), $.CONST(0));
                 }
             };
 
             this.compileParams = function(params, currentLocalStackSize) {
-                var compiler       = this._compiler;
-                var compilerOutput = compiler.getOutput();
+                var compilerOutput = this._compiler.getOutput();
                 var compilerData   = this._compilerData;
 
                 params = wheel.compiler.compilerHelper.splitParams(params);
@@ -155,14 +121,13 @@ console.log('Warning!!!');
                 for (var i = 0; i < params.length; i++) {
                     var param = params[i].trim();
                     if (param !== '') {
-                        var paramInfo    = compilerData.paramInfo(param);
                         var destParam;
-                        var vr           = paramInfo.vr;
-                        var size         = vr ? (vr.size * vr.length) : 1;
+                        var paramInfo = compilerData.paramInfo(param);
+                        var vr        = paramInfo.vr;
+                        var size      = vr ? (vr.size * vr.length) : 1;
 
                         // If the var is declared as a pointer and passed as *varname...
-                        if ((vr && vr.struct && (vr.metaType === $.T_META_POINTER)) &&
-                            (paramInfo.metaType === $.T_META_POINTER)) {
+                        if ((vr && vr.struct && (vr.metaType === $.T_META_POINTER)) && (paramInfo.metaType === $.T_META_POINTER)) {
                             size = vr.struct.size;
                         }
 
@@ -203,11 +168,10 @@ console.log('Warning!!!');
             this.compile = function(line) {
                 $ = wheel.compiler.command;
 
-                var compiler         = this._compiler;
-                var compilerOutput   = compiler.getOutput();
-                var compilerData     = this._compilerData;
-                var i                = line.indexOf('(');
-                var procedure        = line.substr(0, i);
+                var compilerOutput = this._compiler.getOutput();
+                var compilerData   = this._compilerData;
+                var i              = line.indexOf('(');
+                var procedure      = line.substr(0, i);
 
                 if (!wheel.compiler.compilerHelper.validateString(procedure, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_.')) {
                     throw compiler.createError('Syntax error.');
@@ -218,7 +182,7 @@ console.log('Warning!!!');
                 var currentLocalStackSize = compilerData.getLocalOffset();
 
                 if (p !== null) {
-                    callCommand = {code: $.set.code, params: [$.CODE(), {type: $.T_NUM_C, value: p.index - 1}]};
+                    callCommand = {code: $.set.code, params: [$.CODE(), $.CONST(p.index - 1)]};
                 } else {
                     var local = compilerData.findLocal(procedure);
                     if (local !== null) {
@@ -229,27 +193,14 @@ console.log('Warning!!!');
                         // Move the code offset above the stack into the stack of the new procedure...
                         var offset = local.offset + currentLocalStackSize;
                         compilerOutput.a($.set.code, [{type: $.T_NUM_L, value: offset}, {type: $.T_NUM_L, value: local.offset}]);
-                        callCommand = {
-                            code: $.set.code,
-                            params: [
-                                {type: $.T_NUM_G, value: $.REG_CODE},
-                                {type: $.T_NUM_L, value: local.offset}
-                            ]
-                        };
+                        callCommand = {code: $.set.code, params: [$.CODE(), $.LOCAL(local.offset)]};
                     } else {
                         var global = compilerData.findGlobal(procedure)
                         if (global !== null) {
                             if ((global.type !== $.T_NUM_G) && (global.type !== $.T_PROC_G)) {
                                 throw compiler.createError('Type error, can not call "' + procedure + '".');
                             }
-
-                            callCommand = {
-                                code: $.set.code,
-                                params: [
-                                    {type: $.T_NUM_G, value: $.REG_CODE},
-                                    {type: $.T_NUM_G, value: global.offset}
-                                ]
-                            };
+                            callCommand = {code: $.set.code, params: [$.CODE(), $.GLOBAL(global.offset)]};
                         } else {
                             throw compiler.createError('Unknown procedure "' + procedure + '".');
                         }
@@ -258,15 +209,15 @@ console.log('Warning!!!');
                 this.compileParams(line.substr(i + 1, line.length - i - 2).trim(), currentLocalStackSize);
 
                 // Move the code offset to the dest register...
-                compilerOutput.a($.set.code, [$.DEST(),                     $.CODE()]);
+                compilerOutput.a($.set.code, $.DEST(),   $.CODE());
                 // Add 6, these are the call setup commands... (including this one!)
-                compilerOutput.a($.add.code, [$.DEST(),                     {type: $.T_NUM_C, value: 6}]);
+                compilerOutput.a($.add.code, $.DEST(),   $.CONST(6));
 
-                compilerOutput.a($.set.code, [$.SRC(),                      $.STACK()]);
-                compilerOutput.a($.add.code, [$.STACK(),                    {type: $.T_NUM_C, value: currentLocalStackSize}]);
-                compilerOutput.a($.set.code, [{type: $.T_NUM_L, value: 0},  $.SRC()]);
+                compilerOutput.a($.set.code, $.SRC(),    $.STACK());
+                compilerOutput.a($.add.code, $.STACK(),  $.CONST(currentLocalStackSize));
+                compilerOutput.a($.set.code, $.LOCAL(0), $.SRC());
                 // Move the dest register value to the stack, this is the return code offset!
-                compilerOutput.a($.set.code, [{type: $.T_NUM_L, value: 1},  $.DEST()]);
+                compilerOutput.a($.set.code, $.LOCAL(1), $.DEST());
 
                 compilerOutput.add(callCommand);
             };
