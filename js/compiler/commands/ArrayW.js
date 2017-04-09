e1aa3d93e16f220c44a155e843812e5b6b81eb5a
Arno van der Vegt <arnovandervegt@startmail.com>
2017-04-09 14:42:44 +0200

    Removed unused code.

--- js/compiler/commands/ArrayW.js
+++ js/compiler/commands/ArrayW.js
@@ -53,8 +53,7 @@
                     compilerOutput.a($.set.code,  $.SRC(),              $.CONST(localOffset));
                     compilerOutput.a($.add.code,  $.SRC(),              $.STACK());
                     compilerOutput.a($.copy.code, $.CONST(1),           $.CONST(0));
-                } else if (($.isStructVarType(valueParam) && $.isStructVarType(arrayParam)) ||
-                    ($.isNumberType(arrayParam) && $.isStructVarType(valueParam))) {
+                } else  {
                     var size = valueParam.vr.struct.size;
                     compilerOutput.a($.set.code, $.DEST(), indexParam);
                     (size > 1) && compilerOutput.a($.mul.code, $.DEST(), $.CONST(size));
