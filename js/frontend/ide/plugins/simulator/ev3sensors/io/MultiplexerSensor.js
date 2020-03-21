2d8a8f27cbe5a6df265be2adadb6055ff339f3fd
Arno van der Vegt <arnovandervegt@startmail.com>
2020-03-21 13:48:40 +0100

    Fixed Mindstorms sensor multiplexer in simulator.

--- js/frontend/ide/plugins/simulator/ev3sensors/io/MultiplexerSensor.js
+++ js/frontend/ide/plugins/simulator/ev3sensors/io/MultiplexerSensor.js
@@ -24,21 +24,21 @@ exports.MultiplexerSensor = class extends Sensor {
                             type:     Checkbox,
                             ui:       this._ui,
                             tabIndex: this._tabIndex,
-                            onChange: this.onChangeCheckboxValue.bind(this, 0)
+                            onChange: this.onChangeCheckboxValue.bind(this, 0, 4)
                         },
                         {
                             ref:      this.setRef('multiplexerValueInput2'),
                             type:     Checkbox,
                             ui:       this._ui,
                             tabIndex: this._tabIndex + 1,
-                            onChange: this.onChangeCheckboxValue.bind(this, 1)
+                            onChange: this.onChangeCheckboxValue.bind(this, 1, 2)
                         },
                         {
                             ref:      this.setRef('multiplexerValueInput3'),
                             type:     Checkbox,
                             ui:       this._ui,
                             tabIndex: this._tabIndex + 2,
-                            onChange: this.onChangeCheckboxValue.bind(this, 2)
+                            onChange: this.onChangeCheckboxValue.bind(this, 2, 1)
                         }
                     ]
                 }
@@ -48,22 +48,33 @@ exports.MultiplexerSensor = class extends Sensor {
 
     getValue() {
         let refs = this._refs;
-        return (refs.multiplexerValueInput1.getChecked() ? 1 : 0) |
-            (refs.multiplexerValueInput2.getChecked() ? 2 : 0)
-            (refs.multiplexerValueInput3.getChecked() ? 4 : 0);
+        return (
+            (refs.multiplexerValueInput1.getChecked() ? 4 : 0) |
+            (refs.multiplexerValueInput2.getChecked() ? 2 : 0) |
+            (refs.multiplexerValueInput3.getChecked() ? 1 : 0)
+        );
     }
 
-    onChangeVaue(value) {
+    onChangeValue(value) {
         let refs = this._refs;
-        this._value = value;
-        refs.multiplexerValueInput1.setChecked((value & 1) === 1);
+        refs.multiplexerValueInput1.setChecked((value & 4) === 4);
         refs.multiplexerValueInput2.setChecked((value & 2) === 2);
-        refs.multiplexerValueInput3.setChecked((value & 4) === 4);
+        refs.multiplexerValueInput3.setChecked((value & 1) === 1);
+        let state = this._state;
+        let bit   = 1;
+        for (let i = 0; i < 3; i++) {
+            dispatcher.dispatch('Sensor.Multiplexer.Changed', state.getId(), 2 - i, ((value & bit) === bit) ? 1 : 0);
+            bit <<= 1;
+        }
     }
 
-    onChangeCheckboxValue(index, value) {
-        let id = this._sensorContainer.getId();
-        dispatcher.dispatch('Sensor.Multiplexer.Changed', id, index, value ? 1 : 0);
-        this.setTimeoutReset();
+    onChangeCheckboxValue(index, bit, value) {
+        let state = this._state;
+        if (value) {
+            state.setValue(state.getValue() | bit);
+        } else {
+            state.setValue(state.getValue() & (~bit));
+        }
+        dispatcher.dispatch('Sensor.Multiplexer.Changed', state.getId(), index, value ? 1 : 0);
     }
 };
