c7ce6d337d8afe700b493fd866f87bddad4a4a03
Arno van der Vegt <arnovandervegt@startmail.com>
2020-12-05 16:14:56 +0100

    Updated and added test.

--- test/compiler/modules/components/testComponentLabelModule.js
+++ test/compiler/modules/components/testComponentLabelModule.js
@@ -14,10 +14,11 @@ const LIB_FILENAME = 'assets/template/lib/modules/components/label.whl';
 describe(
     'Test Label component module',
     () => {
-        testComponentCall(it, {message: 'Should set hidden',       moduleFile: LIB_FILENAME, procName: 'components.label.setHidden',      property: 'hidden', type: 'number'});
-        testComponentCall(it, {message: 'Should set x',            moduleFile: LIB_FILENAME, procName: 'components.label.setX',           property: 'x',      type: 'number'});
-        testComponentCall(it, {message: 'Should set y',            moduleFile: LIB_FILENAME, procName: 'components.label.setY',           property: 'y',      type: 'number'});
-        testComponentCall(it, {message: 'Should set value number', moduleFile: LIB_FILENAME, procName: 'components.label.setValueNumber', property: 'value',  type: 'number'});
-        testComponentCall(it, {message: 'Should set value string', moduleFile: LIB_FILENAME, procName: 'components.label.setValueString', property: 'value',  type: 'string'});
+        testComponentCall(it, {message: 'Should set hidden',       moduleFile: LIB_FILENAME, procName: 'components.label.setHidden',      property: 'hidden',   type: 'number'});
+        testComponentCall(it, {message: 'Should set x',            moduleFile: LIB_FILENAME, procName: 'components.label.setX',           property: 'x',        type: 'number'});
+        testComponentCall(it, {message: 'Should set y',            moduleFile: LIB_FILENAME, procName: 'components.label.setY',           property: 'y',        type: 'number'});
+        testComponentCall(it, {message: 'Should set font size',    moduleFile: LIB_FILENAME, procName: 'components.label.setFontSize',    property: 'fontSize', type: 'number'});
+        testComponentCall(it, {message: 'Should set value number', moduleFile: LIB_FILENAME, procName: 'components.label.setValueNumber', property: 'value',    type: 'number'});
+        testComponentCall(it, {message: 'Should set value string', moduleFile: LIB_FILENAME, procName: 'components.label.setValueString', property: 'value',    type: 'string'});
     }
 );
