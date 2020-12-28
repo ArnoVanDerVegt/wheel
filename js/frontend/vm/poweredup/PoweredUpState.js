3cc0ec0fdc03de9325561d47bf763167794c32ae
Arno van der Vegt <arnovandervegt@startmail.com>
2020-12-28 13:50:42 +0100

    Moved platform file.

--- js/frontend/vm/poweredup/PoweredUpState.js
+++ js/frontend/vm/poweredup/PoweredUpState.js
@@ -2,6 +2,7 @@
  * Wheel, copyright (c) 2020 - present by Arno van der Vegt
  * Distributed under an MIT license: https://arnovandervegt.github.io/wheel/license.txt
 **/
+const platform                 = require('../../../shared/lib/platform');
 const poweredUpModuleConstants = require('../../../shared/vm/modules/poweredUpModuleConstants');
 const dispatcher               = require('../../lib/dispatcher').dispatcher;
 const getDataProvider          = require('../../lib/dataprovider/dataProvider').getDataProvider;
