739acde2840f1c3ecc9d43c48a090d45a6e156eb
Arno van der Vegt <arnovandervegt@startmail.com>
2021-01-20 12:19:36 +0100

    Added direct control to EV3 motors.

--- js/frontend/ide/plugins/simulator/ev3motors/Plugin.js
+++ js/frontend/ide/plugins/simulator/ev3motors/Plugin.js
@@ -2,6 +2,7 @@
  * Wheel, copyright (c) 2019 - present by Arno van der Vegt
  * Distributed under an MIT license: https://arnovandervegt.github.io/wheel/license.txt
 **/
+const Button     = require('../../../../lib/components/input/Button').Button;
 const dispatcher = require('../../../../lib/dispatcher').dispatcher;
 const DOMNode    = require('../../../../lib/dom').DOMNode;
 const Plugin     = require('../lib/motor/Plugin').Plugin;
@@ -18,5 +19,37 @@ exports.Plugin = class extends Plugin {
         this._device
             .addEventListener('EV3.Connected',    this, this.onDeviceConnected)
             .addEventListener('EV3.Disconnected', this, this.onDeviceDisconnected);
+    }
+
+    initExtra() {
+        return {
+            className: 'flt max-w direct-control',
+            children: [
+                {
+                    type:     Button,
+                    ref:      this.setRef('directControlButton'),
+                    ui:       this._ui,
+                    onClick:  this.onClickDirectControl.bind(this),
+                    uiId:     1,
+                    value:    'Direct control',
+                    color:    'blue',
+                    disabled: true
+                }
+            ]
+        };
+    }
+
+    onClickDirectControl() {
+        dispatcher.dispatch('Menu.EV3.DirectControl');
+    }
+
+    onDeviceConnected() {
+        super.onDeviceConnected();
+        this._refs.directControlButton.setDisabled(false);
+    }
+
+    onDeviceDisconnected() {
+        super.onDeviceDisconnected();
+        this._refs.directControlButton.setDisabled(true);
     }
 };
