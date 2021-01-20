5bc80759bd17c13f2609bb038fbb16987d769483
Arno van der Vegt <arnovandervegt@startmail.com>
2021-01-20 12:31:27 +0100

    Removed/fixed old code.

--- js/shared/devicemodules/SensorModule.js
+++ js/shared/devicemodules/SensorModule.js
@@ -5,95 +5,15 @@
 const sensorModuleConstants = require('../vm/modules/sensorModuleConstants');
 const BasicDeviceModule     = require('./BasicDeviceModule').BasicDeviceModule;
 
-class Sensor {
-    constructor(layer, id, device) {
-        this._layer  = layer;
-        this._id     = id;
-        this._device = device;
-        this._type   = 0;
-        this._mode   = 0;
-    }
-
-    getMode() {
-        return this._mode;
-    }
-
-    setMode(mode) {
-        this._mode = mode;
-    }
-
-    getType() {
-        return this._type;
-    }
-
-    setType(type) {
-        this._type = type;
-    }
-
-    reset() {
-    }
-
-    read() {
-        switch (this._type) {
-            case sensorModuleConstants.SENSOR_TYPE_TOUCH:
-                this._device.readTouchSensor(this._layer, this._id);
-                break;
-            case sensorModuleConstants.SENSOR_TYPE_COLOR:
-                if ((this._mode >= 0) && (this._mode < 3)) {
-                    this._device.readColorSensor(this._layer, this._id, '0' + this._mode);
-                }
-                break;
-            case sensorModuleConstants.SENSOR_TYPE_INFRARED:
-                if ([0, 2].indexOf(this._mode) !== -1) {
-                    this._device.readInfraredSensor(this._layer, this._id, '0' + this._mode);
-                }
-                break;
-            case sensorModuleConstants.SENSOR_TYPE_ULTRASONIC:
-                break;
-            case sensorModuleConstants.SENSOR_TYPE_GYRO:
-                break;
-            case sensorModuleConstants.SENSOR_TYPE_SOUND:
-                break;
-        }
-    }
-}
-
 exports.SensorModule = class extends BasicDeviceModule {
-    constructor(opts) {
-        super(opts);
-        let layerCount    = this._device.getActiveLayerCount();
-        let portsPerLayer = this._device.getPortsPerLayer();
-        this._layers = [];
-        for (let i = 0; i < layerCount; i++) {
-            let sensors = [];
-            for (let j = 0; j < portsPerLayer; j++) {
-                sensors.push(new Sensor(i, j, this._device));
-            }
-            this._layers.push(sensors);
-        }
-    }
-
-    getSensor(layer, id) {
-        return (this._layers[layer] && this._layers[layer][id]) ? this._layers[layer][id] : null;
-    }
-
     run(commandId, data) {
-        let sensor = this.getSensor(data.layer || 0, data.id);
         let device = this._device;
         switch (commandId) {
             case sensorModuleConstants.SENSOR_SET_TYPE:
                 device && device.setType(data.layer, data.id, data.type);
-                sensor && sensor.setType(data.type);
                 break;
             case sensorModuleConstants.SENSOR_SET_MODE:
                 device && device.setMode(data.layer, data.id, data.mode);
-                sensor && sensor.setMode(data.mode);
-                break;
-            case sensorModuleConstants.SENSOR_RESET:
-                sensor && sensor.reset();
-                break;
-            case sensorModuleConstants.SENSOR_READ:
-                sensor && sensor.read();
                 break;
         }
     }
